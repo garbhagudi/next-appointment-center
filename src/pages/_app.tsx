663589c91c86340acd666eb2a89b9ff6de1fad81
Shouri-M <shouri.muralidharan248@gmail.com>
2023-06-16 14:36:51 +0530

    Second Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,19 @@ import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+const {ApolloServer} = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Apollo Server
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const Server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
